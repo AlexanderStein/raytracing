abf827d1bdc71e58e84a4cd772c5ec5941a10b37
Alexander Stein <alexander.stein@mailbox.org>
2022-05-27 23:23:38 +0200

    material: Create a (dummy?) struct and explicit Trait
    Traits can't be stored as members. Traits are interfaces, so this makes
    sense.
    This change must probably needs to be touched again

--- src/material.rs
+++ src/material.rs
@@ -1,5 +1,8 @@
 use crate::{hitable::HitRecord, color::Color, ray::Ray};
 
-pub trait Material {
+#[derive(Clone)]
+pub struct Material;
+
+pub trait MaterialTrait {
     fn scatter(ray_in: &Ray, record: &HitRecord, attenuation: &Color, scattered: &mut Ray) -> bool;
 }
