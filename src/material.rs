5c5db8589e78be5b99f7b43a43bcb5e3ecb34948
Alexander Stein <alexander.stein@mailbox.org>
2022-05-27 23:49:03 +0200

    material: Add metal material

--- src/material.rs
+++ src/material.rs
@@ -32,3 +32,29 @@ impl MaterialTrait for Lambertian {
         true
     }
 }
+
+pub struct Metal {
+    albedo: Color,
+}
+
+impl Metal {
+    pub fn new(color: &Color) -> Self {
+        Metal { albedo: *color }
+    }
+}
+
+impl MaterialTrait for Metal {
+    fn scatter(
+        &self,
+        ray_in: &Ray,
+        record: &HitRecord,
+        attenuation: &mut Color,
+        scattered: &mut Ray,
+        rng: &mut dyn RngCore,
+    ) -> bool {
+        let reflected = ray_in.direction().unit_vector().reflect(&record.normal);
+        *scattered = Ray::new(record.p, reflected);
+        *attenuation = self.albedo;
+        scattered.direction().dot(&record.normal) > 0.0
+    }
+}
