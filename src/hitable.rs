bbe22a1316522021e5f581e4f08eefdfce4d4282
Alexander Stein <alexander.stein@mailbox.org>
2022-05-27 21:08:19 +0200

    hitable: Add HitableList
    With a very similar approach as the C++ code

--- src/hitable.rs
+++ src/hitable.rs
@@ -1,5 +1,7 @@
 use crate::{ray::*, vec3::*};
+use std::rc::Rc;
 
+#[derive(Clone)]
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
@@ -8,6 +10,14 @@ pub struct HitRecord {
 }
 
 impl HitRecord {
+    pub fn new() -> Self {
+        Self {
+            p: Point3::new(0.0, 0.0, 0.0),
+            normal: Vec3::new(0.0, 0.0, 0.0),
+            t: 0.0,
+            front_face: false,
+        }
+    }
     pub fn set_face_normal(&mut self, ray: &Ray, outward_normal: &Vec3) {
         self.front_face = ray.direction().dot(outward_normal) < 0.0;
         self.normal = if self.front_face {
@@ -20,4 +30,26 @@ impl HitRecord {
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, record: &mut HitRecord) -> bool;
+}
+
+pub struct HitableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl Hittable for HitableList {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, record: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::new();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for obj in self.objects.iter() {
+            if obj.hit(ray, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *record = temp_rec.clone();
+            }
+        }
+
+        hit_anything
+    }
 }
