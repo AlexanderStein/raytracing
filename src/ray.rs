523124a78a4d65ad8ffccd15b899cf1df1a2f7ed
Alexander Stein <alexander.stein@mailbox.org>
2022-05-27 19:45:50 +0200

    ray: Adding a sphere
    Add a red sphere at {0, 0, -1} with radius 0.5

--- src/ray.rs
+++ src/ray.rs
@@ -23,8 +23,20 @@ impl Ray {
     }
 
     pub fn ray_color(&self) -> Color {
+        if self.hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5) {
+            return Color::new(1.0, 0.0, 0.0)
+        }
         let unit_direction = unit_vector(&self.direction());
         let t = 0.5 * (unit_direction.y() + 1.0);
         (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    }
+
+    pub fn hit_sphere(&self, center: &Point3, radius: ElemType) -> bool {
+        let oc = self.origin() - *center;
+        let a = self.direction().dot(&self.direction());
+        let b = 2.0 * oc.dot(&self.direction());
+        let c = oc.dot(&oc) - radius*radius;
+        let discriminant = b*b - 4.0*a*c;
+        discriminant > 0.0
     }
 }
