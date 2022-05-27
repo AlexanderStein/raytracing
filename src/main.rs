5f40f5a27653433bd3d8e82ec268760592c51273
Alexander Stein <alexander.stein@mailbox.org>
2022-05-27 21:59:43 +0200

    Add camera module
    This encapsulates camera related code, who would have guessed.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use crate::{color::*, hitable::HitableList, ray::*, sphere::*, vec3::*};
+use crate::{color::*, hitable::HitableList, ray::*, sphere::*, vec3::*, camera::Camera};
 use std::rc::Rc;
 
+mod camera;
 mod color;
 mod hitable;
 mod ray;
@@ -9,9 +10,10 @@ mod vec3;
 
 use image::{load_from_memory_with_format, ImageFormat};
 
+pub const ASPECT_RATIO: f64 = 16.0 / 9.0;
+
 fn main() {
     // Image
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: u32 = 1280;
     const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
 
@@ -21,15 +23,7 @@ fn main() {
     world.objects.push(Rc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    const VIEWPORT_HEIGHT: f64 = 2.0;
-    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
-    const FOCAL_LENGTH: f64 = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+    let camera = Camera::new();
 
     // Render
     let mut pnm_data = String::new();
@@ -44,10 +38,7 @@ fn main() {
         for x in 0..IMAGE_WIDTH {
             let u = x as f64 / (IMAGE_WIDTH - 1) as f64;
             let v = y as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let ray = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
+            let ray = camera.get_ray(u, v);
             let pixel_color = ray.color(&world);
             pnm_data += &pixel_color.pnm_color();
         }
