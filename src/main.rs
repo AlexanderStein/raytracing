42d9436a8e037053e6e33393978f5876e2531942
Alexander Stein <alexander.stein@mailbox.org>
2022-05-28 15:02:26 +0200

    main: add hollow glass sphere
    This is achieved using a negative radius.

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,11 @@ fn main() {
         material_left.clone(),
     )));
     world.objects.push(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.4,
+        material_left.clone(),
+    )));
+    world.objects.push(Rc::new(Sphere::new(
         Point3::new(1.0, 0.0, -1.0),
         0.5,
         material_right.clone(),
