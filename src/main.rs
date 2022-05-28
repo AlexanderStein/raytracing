3cd381548928c2cf8c3286ac72e2d1b5a5a68ea3
Alexander Stein <alexander.stein@mailbox.org>
2022-05-28 15:25:03 +0200

    Revert "main: crate new scene"
    This reverts commit fa7f4f36f21876f8c8c0404c8f165ecb02a6b8b9.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate approx;
 use crate::{camera::Camera, color::*, hitable::HitableList, material::*, sphere::*, vec3::*};
 use image::{load_from_memory_with_format, ImageFormat};
 use rand::prelude::*;
-use std::{rc::Rc, f64::consts::PI};
+use std::rc::Rc;
 
 mod camera;
 mod color;
@@ -25,20 +25,36 @@ fn main() {
     const MAX_DEPTH: usize = 50;
 
     // World
-    let r = (PI/4.0).cos();
     let mut world = HitableList { objects: vec![] };
 
-    let material_left   = Rc::new(Lambertian::new(&Color::new(0.0, 0.0, 1.0)));
-    let material_right  = Rc::new(Lambertian::new(&Color::new(1.0, 0.0, 0.0)));
+    let material_ground = Rc::new(Lambertian::new(&Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(&Color::new(0.1, 0.2, 0.5)));
+    let material_left   = Rc::new(Dielectric::new(1.5));
+    let material_right  = Rc::new(Metal::new(&Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.objects.push(Rc::new(Sphere::new(
-        Point3::new(-r, 0.0, -1.0),
-        r,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground.clone(),
+    )));
+    world.objects.push(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        material_center.clone(),
+    )));
+    world.objects.push(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
         material_left.clone(),
     )));
     world.objects.push(Rc::new(Sphere::new(
-        Point3::new(r, 0.0, -1.0),
-        r,
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.4,
+        material_left.clone(),
+    )));
+    world.objects.push(Rc::new(Sphere::new(
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         material_right.clone(),
     )));
 
