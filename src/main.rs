8a40933bfb62cf90de9f637e2ad25f3267e77b66
Alexander Stein <alexander.stein@mailbox.org>
2022-05-28 10:36:57 +0200

    main: insert two glass spheres

--- src/main.rs
+++ src/main.rs
@@ -29,8 +29,8 @@ fn main() {
     let mut world = HitableList { objects: vec![] };
 
     let material_ground = Box::new(Lambertian::new(&Color::new(0.8, 0.8, 0.0)));
-    let material_center = Box::new(Lambertian::new(&Color::new(0.7, 0.3, 0.3)));
-    let material_left = Box::new(Metal::new(&Color::new(0.8, 0.8, 0.8), 0.3));
+    let material_center = Box::new(Dielectric::new(1.5));
+    let material_left = Box::new(Dielectric::new(1.5));
     let material_right = Box::new(Metal::new(&Color::new(0.8, 0.6, 0.2), 1.0));
 
     world.objects.push(Rc::new(Sphere::new(
