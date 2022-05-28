105f9faeeed45af252d62cf2c380a55f0f993db0
Alexander Stein <alexander.stein@mailbox.org>
2022-05-28 11:13:54 +0200

    material: address total internal reflection

--- src/material.rs
+++ src/material.rs
@@ -101,8 +101,16 @@ impl MaterialTrait for Dielectric {
         };
 
         let unit_direction = ray.direction().unit_vector();
-        let refracted = unit_direction.refract(&record.normal, refraction_ratio);
-        let scattered = Ray::new(record.p, refracted);
+        let cos_theta = (-unit_direction).dot(&record.normal).min(1.0);
+        let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let direction = if cannot_refract {
+            unit_direction.reflect(&record.normal)
+        } else {
+            unit_direction.refract(&record.normal, refraction_ratio)
+        };
+        let scattered = Ray::new(record.p, direction);
         Some((attenuation, scattered))
     }
 }
