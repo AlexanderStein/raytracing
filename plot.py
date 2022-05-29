393982d24e812e70e26a23cd51612b0d10a1cdd9
Alexander Stein <alexander.stein@mailbox.org>
2022-05-29 10:13:04 +0200

    plot.py: Support plotting multiple files

--- plot.py
+++ plot.py
@@ -5,29 +5,29 @@ import json
 from bokeh.plotting import figure, show
 
 def main():
-    #TODO: plot multiple result files using nargs='+',
     parser = argparse.ArgumentParser(description='Plot hyperfine results')
-    parser.add_argument('file', metavar='path', type=str,
+    parser.add_argument('files', metavar='path', type=str, nargs='+',
                         help='Path to hpyerfine JSON output file')
 
     args = parser.parse_args()
 
     p = figure(title="Raytracing time", x_axis_label="# of threads", y_axis_label="time [s]")
 
-    with open(args.file) as file:
-        data = json.load(file)
-        threads = []
-        time = []
-        try:
-            id = data['env']['id']
-        except:
-            id = "unknown"
+    for file in args.files:
+        with open(file) as file:
+            data = json.load(file)
+            threads = []
+            time = []
+            try:
+                id = data['env']['id']
+            except:
+                id = "unknown"
 
-        for run in data['results']:
-            time.append(float(run['mean']))
-            threads.append(int(run['parameters']['threads']))
+            for run in data['results']:
+                time.append(float(run['mean']))
+                threads.append(int(run['parameters']['threads']))
 
-        p.line(threads, time, legend_label=f'{id}', line_width=2)
+            p.line(threads, time, legend_label=f'{id}', line_width=2)
 
     show(p)
 
