d60ceb9a78e18d4936100fa7a577c1cde7eff1f2
Alexander Stein <alexander.stein@mailbox.org>
2022-05-29 19:56:48 +0200

    world: Simplify world creation using new methods

--- src/world.rs
+++ src/world.rs
@@ -3,14 +3,14 @@ use rand::{Rng, RngCore};
 
 pub fn random_scene(rng: &mut dyn RngCore) -> HitableList {
     // TODO: Create Vec<Box<dyn Hittable>> first and pass this to world
-    let mut world = HitableList { objects: vec![] };
+    let mut world = HitableList::new();
 
     let ground_material = Box::new(Lambertian::new(&Color::new(0.5, 0.5, 0.5)));
-    world.objects.push(Box::new(Sphere::new(
+    world.push(Sphere::new(
         Point3::new(0.0, -1000.0, 0.0),
         1000.0,
         ground_material,
-    )));
+    ));
 
     for a in -11..11 {
         for b in -11..11 {
@@ -35,33 +35,19 @@ pub fn random_scene(rng: &mut dyn RngCore) -> HitableList {
                     // glass
                     Box::new(Dielectric::new(1.5))
                 };
-                world
-                    .objects
-                    .push(Box::new(Sphere::new(center, 0.2, sphere_material)));
+                world.push(Sphere::new(center, 0.2, sphere_material));
             }
         }
     }
 
     let material1 = Box::new(Dielectric::new(1.5));
-    world.objects.push(Box::new(Sphere::new(
-        Point3::new(0.0, 1.0, 0.0),
-        1.0,
-        material1,
-    )));
+    world.push(Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, material1));
 
     let material2 = Box::new(Lambertian::new(&Color::new(0.4, 0.2, 0.1)));
-    world.objects.push(Box::new(Sphere::new(
-        Point3::new(-4.0, 1.0, 0.0),
-        1.0,
-        material2,
-    )));
+    world.push(Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, material2));
 
     let material3 = Box::new(Metal::new(&Color::new(0.7, 0.6, 0.5), 0.0));
-    world.objects.push(Box::new(Sphere::new(
-        Point3::new(4.0, 1.0, 0.0),
-        1.0,
-        material3,
-    )));
+    world.push(Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, material3));
 
     world
 }
