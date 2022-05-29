1b439feeff71f6f4e60603c251e20e2f2c1f852c
Alexander Stein <alexander.stein@mailbox.org>
2022-05-29 11:43:51 +0200

    plot: Add linear gain line
    This line indicates a pure (theoretical) linear scaling.

--- plot.py
+++ plot.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+from turtle import color
 from bokeh.layouts import row
 from bokeh.plotting import figure, show
 from bokeh.models import ColumnDataSource, Range1d
@@ -19,6 +20,7 @@ def main():
     speedup_plot = figure(
         title="Performance gain", x_axis_label="# of threads", y_axis_label="Speedup factor")
 
+    max_threads = 0
     for file in args.files:
         with open(file) as file:
             data = json.load(file)
@@ -34,6 +36,7 @@ def main():
                 num_threads = int(run['parameters']['threads'])
                 time.append(mean)
                 threads.append(num_threads)
+                max_threads = max(max_threads, num_threads)
 
             data = {
                 'time': time,
@@ -70,8 +73,22 @@ def main():
                                 fill_color="white",
                                 size=8)
 
+    # Add 1 to have a small margin to the right border
+    speedup_axis_max = max_threads + 1
+    linear = {
+        'threads': [0, speedup_axis_max],
+        'gain': [0, speedup_axis_max]
+    }
+    speedup_plot.line(x='threads', y='gain',
+                        source=ColumnDataSource(data=linear),
+                        color='green',
+                        alpha=0.3,
+                        line_width=1)
+
     # Put legend to top left and the plot into top right corner
     speedup_plot.legend.location = "top_left"
+    speedup_plot.x_range = Range1d(0, speedup_axis_max)
+    speedup_plot.y_range = Range1d(0, speedup_axis_max)
     show(row(children=[time_plot, speedup_plot], sizing_mode="scale_width"))
 
 
