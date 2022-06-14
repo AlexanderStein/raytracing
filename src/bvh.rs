7c42e3835a6c51ee9f136840d00d21b1c10cfd6f
Alexander Stein <alexander.stein@mailbox.org>
2022-06-14 18:42:56 +0200

    bvh: Implement new() method

--- src/bvh.rs
+++ src/bvh.rs
@@ -1,4 +1,5 @@
 use crate::{aabb::AABB, hitable::*, ray::Ray};
+use std::cmp::Ordering;
 
 enum BVHNode {
     Branch { left: Box<BVH>, right: Box<BVH> },
@@ -11,8 +12,77 @@ pub struct BVH {
 }
 
 impl BVH {
-    pub fn new(mut hitable: Vec<Box<dyn Hittable>>, time0: f64, time1: f64) -> Self {
-        unimplemented!()
+    pub fn new(mut objects: Vec<Box<dyn Hittable>>, time0: f64, time1: f64) -> Self {
+        fn box_compare(
+            time0: f64,
+            time1: f64,
+            axis: usize,
+        ) -> impl FnMut(&Box<dyn Hittable>, &Box<dyn Hittable>) -> Ordering {
+            move |a, b| {
+                let a_box = a.bounding_box(time0, time1);
+                let b_box = b.bounding_box(time0, time1);
+                if let (Some(a), Some(b)) = (a_box, b_box) {
+                    a.min()[axis].partial_cmp(&b.min()[axis]).unwrap()
+                } else {
+                    panic!("no bounding box in bvh node")
+                }
+            }
+        }
+
+        fn axis_range(
+            objects: &Vec<Box<dyn Hittable>>,
+            time0: f64,
+            time1: f64,
+            axis: usize,
+        ) -> f64 {
+            let (min, max) = objects
+                .iter()
+                .fold((f64::MIN, f64::MAX), |(bmin, bmax), hit| {
+                    if let Some(bbox) = hit.bounding_box(time0, time1) {
+                        (bmin.min(bbox.min()[axis]), bmin.max(bbox.max()[axis]))
+                    } else {
+                        (bmin, bmax)
+                    }
+                });
+            max - min
+        }
+
+        let mut axis_ranges: Vec<(usize, f64)> = (0..3)
+            .map(|a| (a, axis_range(&objects, time0, time1, a)))
+            .collect();
+
+        axis_ranges.sort_unstable_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+        let axis = axis_ranges[0].0;
+
+        objects.sort_by(box_compare(time0, time1, axis));
+        let len = objects.len();
+        match len {
+            0 => panic!("no elements in scene"),
+            1 => {
+                let leaf = objects.pop().unwrap();
+                if let Some(bbox) = leaf.bounding_box(time0, time1) {
+                    BVH {
+                        tree: BVHNode::Leaf(leaf),
+                        bbox,
+                    }
+                } else {
+                    panic!("no bounding box in bvh node")
+                }
+            }
+            _ => {
+                let right = BVH::new(objects.drain(len / 2..).collect(), time0, time1);
+                let left = BVH::new(objects, time0, time1);
+                let bbox = AABB::surrounding_box(&left.bbox, &right.bbox);
+
+                BVH {
+                    tree: BVHNode::Branch {
+                        left: Box::new(left),
+                        right: Box::new(right),
+                    },
+                    bbox,
+                }
+            }
+        }
     }
 }
 
