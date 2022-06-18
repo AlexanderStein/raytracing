5f1c7a6eba55f20c757308b8d8d30875a38e03b8
Alexander Stein <alexander.stein@mailbox.org>
2022-06-18 09:19:08 +0200

    texture: Add CheckerTexture

--- src/texture.rs
+++ src/texture.rs
@@ -21,3 +21,25 @@ impl Texture for SolidColor {
         self.color
     }
 }
+
+pub struct CheckerTexture {
+    odd: Box<dyn Texture>,
+    even: Box<dyn Texture>,
+}
+
+impl CheckerTexture {
+    pub fn new(even: Box<dyn Texture>, odd: Box<dyn Texture>) -> Self {
+        Self { odd, even }
+    }
+}
+
+impl Texture for CheckerTexture {
+    fn value(&self, p: &Point3<f64>) -> Color {
+        let sines = (10.0 * p.x).sin() * (10.0 * p.y).sin() * (10.0 * p.z).sin();
+        if sines < 0.0 {
+            self.odd.value(p)
+        } else {
+            self.even.value(p)
+        }
+    }
+}
