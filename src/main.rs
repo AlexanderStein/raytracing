da2f9e8886fb8ec47cdde5e4b11e4044abbd7b36
Alexander Stein <alexander.stein@mailbox.org>
2022-06-19 12:52:41 +0200

    main: Add new scene 3: Earth globe

--- src/main.rs
+++ src/main.rs
@@ -126,6 +126,28 @@ fn main() {
                 aperture,
             )
         }
+        3 => {
+            let lookfrom = Point3 {
+                x: 13.0,
+                y: 2.0,
+                z: 3.0,
+            };
+            let lookat = Point3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            };
+            let vfov = 20.0;
+            let aperture = 0.0;
+
+            (
+                earth(&mut rng),
+                lookfrom,
+                lookat,
+                vfov,
+                aperture,
+            )
+        }
         _ => {
             let lookfrom = Point3 {
                 x: 13.0,
