2bdfc5c0f2cecb91aa6c052749444990fffc9a85
Alexander Stein <alexander.stein@mailbox.org>
2022-06-19 11:50:14 +0200

    perlin: Use random vector for lattice points

--- src/perlin.rs
+++ src/perlin.rs
@@ -1,7 +1,17 @@
-use cgmath::Point3;
+use cgmath::{InnerSpace, Point3, Vector3};
 use rand::{Rng, RngCore};
 
 const POINT_COUNT: usize = 256;
+
+fn generate(rng: &mut dyn RngCore) -> Vec<Vector3<f64>> {
+    (0..POINT_COUNT)
+        .map(|_| Vector3 {
+            x: rng.gen_range(-1.0..1.0),
+            y: rng.gen_range(-1.0..1.0),
+            z: rng.gen_range(-1.0..1.0),
+        })
+        .collect()
+}
 
 fn generate_perm(rng: &mut dyn RngCore) -> Vec<usize> {
     let mut p: Vec<usize> = (0..POINT_COUNT).collect();
@@ -14,22 +24,26 @@ fn generate_perm(rng: &mut dyn RngCore) -> Vec<usize> {
     p
 }
 
-fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+fn trilinear_interp(c: &[[[Vector3<f64>; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    let uu = u * u * (3.0 - 2.0 * u);
+    let vv = v * v * (3.0 - 2.0 * v);
+    let ww = w * w * (3.0 - 2.0 * w);
     c.iter().enumerate().fold(0.0, |accum, (i, i_iter)| {
         i_iter.iter().enumerate().fold(accum, |accum, (j, j_iter)| {
             j_iter.iter().enumerate().fold(accum, |accum, (k, k_iter)| {
+                let weight_v = Vector3::new(u - i as f64, v - j as f64, w - k as f64);
                 accum
-                    + (i as f64 * u + (1 - i) as f64 * (1.0 - u))
-                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
-                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
-                        * *k_iter
+                    + (i as f64 * uu + (1 - i) as f64 * (1.0 - uu))
+                        * (j as f64 * vv + (1 - j) as f64 * (1.0 - vv))
+                        * (k as f64 * ww + (1 - k) as f64 * (1.0 - ww))
+                        * k_iter.dot(weight_v)
             })
         })
     })
 }
 #[derive(Clone)]
 pub struct Perlin {
-    ranfloat: Vec<f64>,
+    ranfloat: Vec<Vector3<f64>>,
     perm_x: Vec<usize>,
     perm_y: Vec<usize>,
     perm_z: Vec<usize>,
@@ -37,13 +51,8 @@ pub struct Perlin {
 
 impl Perlin {
     pub fn new(rng: &mut dyn RngCore) -> Self {
-        let ranfloat = rng
-            .sample_iter(rand::distributions::Standard)
-            .take(POINT_COUNT)
-            .collect();
-
         Self {
-            ranfloat,
+            ranfloat: generate(rng),
             perm_x: generate_perm(rng),
             perm_y: generate_perm(rng),
             perm_z: generate_perm(rng),
@@ -61,7 +70,7 @@ impl Perlin {
         let i = p.x.floor() as i32;
         let j = p.y.floor() as i32;
         let k = p.z.floor() as i32;
-        let mut c = [[[0.0; 2]; 2]; 2];
+        let mut c = [[[Vector3::new(0.0, 0.0, 0.0); 2]; 2]; 2];
         c.iter_mut().enumerate().for_each(|(di, i_iter)| {
             i_iter.iter_mut().enumerate().for_each(|(dj, j_iter)| {
                 j_iter.iter_mut().enumerate().for_each(|(dk, k_iter)| {
