a3af1b6e0b840a6011d1bd8ecfcc213a26f848db
Alexander Stein <alexander.stein@mailbox.org>
2022-06-19 11:35:12 +0200

    perlin Add trilinear interpolation

--- src/perlin.rs
+++ src/perlin.rs
@@ -14,6 +14,19 @@ fn generate_perm(rng: &mut dyn RngCore) -> Vec<usize> {
     p
 }
 
+fn trilinear_interp(c: &[[[f64; 2]; 2]; 2], u: f64, v: f64, w: f64) -> f64 {
+    c.iter().enumerate().fold(0.0, |accum, (i, i_iter)| {
+        i_iter.iter().enumerate().fold(accum, |accum, (j, j_iter)| {
+            j_iter.iter().enumerate().fold(accum, |accum, (k, k_iter)| {
+                accum
+                    + (i as f64 * u + (1 - i) as f64 * (1.0 - u))
+                        * (j as f64 * v + (1 - j) as f64 * (1.0 - v))
+                        * (k as f64 * w + (1 - k) as f64 * (1.0 - w))
+                        * *k_iter
+            })
+        })
+    })
+}
 #[derive(Clone)]
 pub struct Perlin {
     ranfloat: Vec<f64>,
@@ -38,12 +51,24 @@ impl Perlin {
     }
 
     pub fn noise(&self, p: &Point3<f64>) -> f64 {
-        // Cast to usize _after_ doing & operation,
-        // otherwise negative coordinates result in index 0
-        let i = ((4.0 * p.x) as i32 & 255) as usize;
-        let j = ((4.0 * p.y) as i32 & 255) as usize;
-        let k = ((4.0 * p.z) as i32 & 255) as usize;
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
 
-        self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+        let mut c = [[[0.0; 2]; 2]; 2];
+        c.iter_mut().enumerate().for_each(|(di, i_iter)| {
+            i_iter.iter_mut().enumerate().for_each(|(dj, j_iter)| {
+                j_iter.iter_mut().enumerate().for_each(|(dk, k_iter)| {
+                    *k_iter = self.ranfloat[self.perm_x[((i + di as i32) & 255) as usize]
+                        ^ self.perm_y[((j + dj as i32) & 255) as usize]
+                        ^ self.perm_z[((k + dk as i32) & 255) as usize]]
+                })
+            })
+        });
+
+        return trilinear_interp(&c, u, v, w);
     }
 }
