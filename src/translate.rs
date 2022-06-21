b8e025e15c7c2e17e5154f8ae7aad9dd56718d78
Alexander Stein <alexander.stein@mailbox.org>
2022-06-21 23:14:59 +0200

    translate: format document

--- src/translate.rs
+++ src/translate.rs
@@ -7,7 +7,12 @@ pub struct Translate<H: Hittable> {
 }
 
 impl<H: Hittable> Hittable for Translate<H> {
-    fn hit(&self, ray: &crate::ray::Ray, t_min: f64, t_max: f64) -> Option<crate::hitable::HitRecord> {
+    fn hit(
+        &self,
+        ray: &crate::ray::Ray,
+        t_min: f64,
+        t_max: f64,
+    ) -> Option<crate::hitable::HitRecord> {
         let moved_ray = Ray::new(ray.origin() - self.offset, ray.direction(), ray.time());
         self.hitable.hit(ray, t_min, t_max).map(|mut record| {
             record.p += self.offset;
@@ -17,8 +22,8 @@ impl<H: Hittable> Hittable for Translate<H> {
     }
 
     fn bounding_box(&self, time0: f64, time1: f64) -> Option<crate::aabb::AABB> {
-        self.hitable.bounding_box(time0, time1).map(|bbox| {
-            AABB::new(bbox.min(), bbox.max())
-        })
+        self.hitable
+            .bounding_box(time0, time1)
+            .map(|bbox| AABB::new(bbox.min(), bbox.max()))
     }
 }
